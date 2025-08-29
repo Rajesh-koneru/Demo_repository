7d01accfd4b224743763adade96e9acd64d2bbb4
Rajesh-koneru <rajeshkoneru1820@gmail.com>
2025-08-29 11:01:57 +0530

    demo commit

---
+++ Demo.js
@@ -0,0 +1,11 @@
+// java script code 
+
+var a=5
+function add(){
+    var a=3
+    console.log("the value of a inside the function ",a);
+    return a
+
+};
+add()
+console.log("the value of a outside the function...",add());
---
+++ python.py
@@ -0,0 +1,8 @@
+def isPalindrome(x: int) -> bool:
+        int_str=str(x)
+        reverse_str=''
+        for i in range(len(int_str)-1):
+            print(int_str[-i])
+            reverse_str+=int_str[-i]
+
+isPalindrome(535)
